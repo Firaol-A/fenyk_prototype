112d94292ead61b9cbb428fa4eb32e0f08b100c8
FIraol-A <firaol.ahmed@edu.sait.ca>
2025-11-29 16:49:14 -0700

    Fixed Error: Page unable to load if signout was click while on the Profile page

--- src/app/components/app-layout.js
+++ src/app/components/app-layout.js
@@ -2,85 +2,94 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { useAuth } from "../firebase/auth-context";
-import { useRouter } from "next/navigation";
+import {useAuth} from "@/app/_utils/auth-context";
+import {useRouter} from "next/navigation";
+import {Suspense} from "react";
 
-export default function AppLayout({ children }) {
+export default function AppLayout({children}) {
 
-    const { logout } = useAuth();
+    const {logout} = useAuth();
     const router = useRouter();
 
     const navItems = [
-        {navName:'SCHEDULE', navPath:'/icons/calendar-event-fill.svg'},
-        {navName:'NOTICES', navPath:'/icons/megaphone-fill.svg'},
-        {navName:'CALENDAR', navPath:'/icons/pin-angle-fill.svg'},
-        {navName:'FILES', navPath:'/icons/folder-fill.svg'},
+        {navName: 'SCHEDULE', navPath: '/icons/calendar-event-fill.svg'},
+        {navName: 'NOTICES', navPath: '/icons/megaphone-fill.svg'},
+        {navName: 'CALENDAR', navPath: '/icons/pin-angle-fill.svg'},
+        {navName: 'FILES', navPath: '/icons/folder-fill.svg'},
     ]
 
     function getNavItems(item) {
         return (
-          <div className="" id={item.id}>
-              <Link href={item.navName.toLowerCase()} className={"flex flex-row items-center mb-5"}>
-                  <Image
-                      src={item.navPath}
-                      alt={""}
-                      width={32}
-                      height={32}
-                      className="mr-2 "
-                  />
-                  <p>{item.navName}</p>
-              </Link>
-          </div>
+            <div className={"flex flex-row items-center mb-5 hover:cursor-pointer"} id={item.id}
+                 onClick={() => router.push(item.navName)}>
+                <Image
+                    src={item.navPath}
+                    alt={""}
+                    width={32}
+                    height={32}
+                    className="mr-2"
+                />
+                <p>{item.navName}</p>
+            </div>
         );
     }
 
-  const handleLogout = async () => {
-    await logout();
-    router.push("/login");
-  };
-  return (
-    <div className="h-screen w-screen flex">
-      {/* nav bar */}
-        <div className="w-45 bg-[#0D2636] text-white flex flex-col items-center py-6">
-            <Link href={"/home"}>
-                <Image
-                    src="/images/Fenyk_logo_2.png"
-                    alt="FENYK Logo"
-                    width={150}
-                    height={150}
-                    className="mb-8"
-                />
-            </Link>
-            <ul>
-                {navItems.map(getNavItems)}
-            </ul>
-            <button
-                onClick={handleLogout}
-                className="mt-auto bg-[#F6F6F6] text-black font-bold px-4 py-3 rounded-lg hover:opacity-90 transition w-40"
-            >
-                SIGN OUT
-            </button>
-        </div>
-
-        {/* top bar, content*/}
-        <div className="flex-1 flex flex-col">
-        {/* top bar */}
-            <div className="h-20 w-full bg-[#0D2636] flex items-center px-5 text-white justify-end  ">
-                <Link href="/profile">
+    const handleLogout = async () => {
+        await logout();
+        router.push("/login");
+    };
+    return (
+        <div className="h-screen w-screen flex">
+            {/* nav bar */}
+            <div className="w-45 bg-[#0D2636] text-white flex flex-col items-center py-6">
+                <Link href={"/home"}>
                     <Image
-                        src={"icons/person-circle.svg"}
-                        alt=""
-                        width={50}
-                        height={50}
-                        className="fill-[#F6F6F6] active:fill-[#668F80]"
+                        src="/images/Fenyk_logo_2.png"
+                        alt="FENYK Logo"
+                        width={150}
+                        height={150}
+                        className="mb-8"
                     />
                 </Link>
+                <ul>
+                    {navItems.map(getNavItems)}
+                </ul>
+                <button
+                    onClick={handleLogout}
+                    className="mt-auto bg-[#F6F6F6] text-black font-bold px-4 py-3 rounded-lg hover:opacity-90 transition w-40 hover:cursor-pointer"
+                >
+                    SIGN OUT
+                </button>
             </div>
-            {/* main content goes here */}
-            <div className="flex-1 bg-[#F6F6F6] overflow-y-auto">
-              {children}
+
+            {/* top bar, content*/}
+            <div className="flex-1 flex flex-col">
+                {/* top bar */}
+                <div className="h-20 w-full bg-[#0D2636] flex items-center px-5 text-white justify-end gap-7">
+                    <Image
+                        onClick={() => router.push("/notifications")}
+                        src={"icons/bell-fill.svg"}
+                        alt={""}
+                        width={40}
+                        height={40}
+                        className="hover:cursor-pointer"
+                    />
+                    <Image
+                        onClick={() => router.push("/profile")}
+                        src={"icons/person-circle.svg"}
+                        alt=""
+                        width={40}
+                        height={40}
+                        className="hover:cursor-pointer"
+                    />
+                </div>
+                {/* main content goes here */}
+                <div className="flex-1 bg-[#F6F6F6] overflow-y-auto">
+                    <Suspense fallback={null}>
+                        {children}
+                    </Suspense>
+                </div>
             </div>
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
